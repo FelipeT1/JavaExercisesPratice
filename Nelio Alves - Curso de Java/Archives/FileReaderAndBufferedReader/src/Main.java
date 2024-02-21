049867a4979dffd3b65748f6a5cfa8e277e39208
FelipeT <felipeteixeiramello99@gmail.com>
2024-02-21 09:51:51 -0300

    FileReader and BufferedReader

---
+++ Nelio Alves - Curso de Java/Archives/FileReaderAndBufferedReader/src/Main.java
@@ -0,0 +1,9 @@
+import java.io.FileReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader()
+    }
+}
